1681fb19d933baaa7a6d1c9b91d43f678afc1222
sk87125 <chijclasss@gmail.com>
2016-12-10 19:24:22 +0800

    使用Json模式存檔 但是資料一但超過100w筆會有記憶體問題

--- final_db_insert.py
+++ final_db_insert.py
@@ -3,20 +3,19 @@ import json
 import pickledb
 
 C_id = 1
-#dict2 = {}
-dict = {}
-dict2 = {}
-
+dict = {"id":' ',"title":' ',"published":' ',"content":' ',"category":' ',"duration":0,"favoriteCount":0,"$viewCount":0,"author":' ',"keyword":' ',"_uid":0}
+add_dict2={}
+dict2 = dict.copy()
 #Please try to create the youtube table structure first
 
 
 
-db = pickledb.load('youtube.db', True) 
-file = open('youtube.gais')
+db = pickledb.load('youtube1.db', False) 
+file = open('youtube_1.gais')
+
 for line in file:
-	#print line
-	data_String = line.lstrip( '@' );	
-	db.dcreate(C_id)
+	data_String = line.lstrip( '@' );		
+	#db.dcreate(C_id)
 	if data_String != '\n':
 		data_list = data_String.split(':')
 		key_String = data_list[0]		
@@ -24,50 +23,31 @@ for line in file:
 		value_String=value_String.rstrip( '\n' );
 		#print key_String		
 		#print value_String		
-		youtube_line = (key_String,value_String}
-		print youtube_line
-		#dict.update(youtube_line)
-		db.dadd(C_id,key_String,value_String)
+		youtube_line = {key_String:value_String}
+		#print youtube_line
+		dict2[key_String] = value_String
+		#db.dadd(C_id,youtube_line)
 		#db.set(key_String, value_String) 
 		if key_String=="_uid":
-			#add_dict2 = {C_id:dict}
+			add_dict = {C_id:dict2}			
 			C_id = C_id+1
-			#print dict
-			print '\n\n\n\n\n'			
-			#dict2.update(add_dict2)
+			if C_id % 20 == 0:
+				jsObj = json.dumps(add_dict2) 
+			print C_id
+			print '\n\n\n\n\n'
+			add_dict2.update(add_dict.copy())
+			dict2 = dict.copy()
+			if C_id % 10000 == 0:
+				break;
+			
+			
 	
-	
-print dict2
+print add_dict2
 file.close()
-db.dump()
-	
-#db_string = input_string.split()
-"""if db_string[0] == "exit":
-	break
-if db_string[0] == "INSERT":	
-	print "Insrt success!!"
-	print db_string
-	key_String = db_string[3].lstrip( '(' );
-	key_String = key_String.rstrip( ')' );
-	key_list = key_String.split(',')
-	print key_list
-		
-		
-		split1 = x.split(' ')
-		split2 = split1[3].split(',')
-		split3 = split1[5].split(',')
+jsObj = json.dumps(add_dict2)  
 
-		for i in range(0,len(split2)):
-			add_dict = {split2[i]:split3[i]}
-			dict.update(add_dict)
-		
-	add_dict2 = {C_id:dict}
-	C_id += 1
-	dict2.update(add_dict2)
-	print (dict2)
-	
-	jsObj = json.dumps(dict2)  
-	  
-	fileObject = open('db.json', 'w')  
-	fileObject.write(jsObj)  
-	fileObject.close()  """
+fileObject = open('youtube.db', 'w')  
+fileObject.write(jsObj)  
+fileObject.close()  
+#db.dump()	
+#db_string = input_string.split()
